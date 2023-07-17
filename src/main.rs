070126067da7be13f954717204e3028a1785b0ce
radlinskii <radlinskiignacy@gmail.com>
2023-07-17 22:52:06 +0200

    feat: add clap library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 mod cli;
 
+use clap::Parser;
 use cli::print::print_hello;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
     print_hello();
+
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
