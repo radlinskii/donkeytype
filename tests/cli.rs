9e6764a044c341f35d0752164e51e8bf22fd6937
radlinskii <radlinskiignacy@gmail.com>
2023-08-03 16:05:17 +0200

    enable test again

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,23 +3,23 @@ use std::io::Write;
 use assert_cmd::Command;
 use predicates;
 
-// #[test]
-// fn should_print_default_expected_input() {
-//     let mut temp_dict_file = tempfile::NamedTempFile::new().expect("Unable to create temp file");
-//     temp_dict_file
-//         .write_all(r#"hello world"#.as_bytes())
-//         .expect("Unable to write to temp file");
+#[test]
+fn should_print_default_expected_input() {
+    let mut temp_dict_file = tempfile::NamedTempFile::new().expect("Unable to create temp file");
+    temp_dict_file
+        .write_all(r#"hello world"#.as_bytes())
+        .expect("Unable to write to temp file");
 
-//     let mut cmd = Command::cargo_bin("donkeytype").expect("Unable to get cargo bin");
-//     let cmd = cmd.arg(format!(
-//         "--dictionary-path={}",
-//         temp_dict_file.path().display()
-//     ));
+    let mut cmd = Command::cargo_bin("donkeytype").expect("Unable to get cargo bin");
+    let cmd = cmd.arg(format!(
+        "--dictionary-path={}",
+        temp_dict_file.path().display()
+    ));
 
-//     cmd.assert()
-//         .success()
-//         .stdout(predicates::str::contains("hello"));
-// }
+    cmd.assert()
+        .success()
+        .stdout(predicates::str::contains("hello"));
+}
 
 #[test]
 fn should_print_help_message_for_normal_mode() {
