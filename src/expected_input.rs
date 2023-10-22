7df5182a25e4cdea47a7a229614585b2eb224041
MercMayhem <amanrao032@gmail.com>
2023-10-22 14:13:31 +0530

    made function get_string_till_space, added tests

--- src/expected_input.rs
+++ src/expected_input.rs
@@ -121,6 +121,7 @@ fn create_uppercase_words(
 #[automock]
 pub trait ExpectedInputInterface {
     fn get_string(&self, len: usize) -> String;
+    fn get_string_till_space(&self, len:usize) -> String;
 }
 
 impl ExpectedInputInterface for ExpectedInput {
@@ -134,6 +135,27 @@ impl ExpectedInputInterface for ExpectedInput {
         let (s, _) = split_by_char_index(&s, len);
 
         s.to_string()
+    }
+
+    /// Cuts string saved in ExpectedInput till space present at or after specified length instance and returns it
+    ///
+    /// If string is shorter than the specified length it repeats it enough times for it to be long
+    /// enough.
+    fn get_string_till_space(&self, len: usize) -> String {
+        if len == 0{
+            return "".to_string();
+        }
+        
+        let s = self.str.clone() + " ";
+        let s = s.repeat((len / s.chars().count()) as usize + 1);
+        
+        let space_index = s[len-1..]
+            .find(" ")
+            .map(|i| i + len - 1)
+            .unwrap_or_else(|| len - 1);
+
+        return s[..=space_index].to_string();
+
     }
 }
 
@@ -229,5 +251,24 @@ mod tests {
         };
 
         assert_eq!(expected_input.get_string(5), "Բարեւ");
+    }
+
+
+    #[test]
+    fn testing_get_string_till_space() {
+        let expected_input = ExpectedInput{
+            str: "She Sells Sea Shells on the Sea Shore".to_string()
+        };
+
+        assert_eq!(expected_input.get_string_till_space(15), "She Sells Sea Shells ")
+    }
+
+    #[test]
+    fn testing_get_string_till_space2() {
+        let expected_input = ExpectedInput{
+            str: "She Sells Sea Shells on the Sea Shore".to_string()
+        };
+
+        assert_eq!(expected_input.get_string_till_space(9), "She Sells ")
     }
 }
